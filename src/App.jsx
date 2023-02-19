3feb67e934f3133630706dd1103a0fa449145811
Bogdan Gheorghe <dodebogdan@yahoo.com>
2023-02-19 22:42:47 +0200

    Created counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+  };
 
   return (
-    <h1>Hello word</h1>
-  )
+    <>
+      <div>
+        <h1 className="center-text">Alexandru Chiricea</h1>
+        <h2 className="center-text">Personal trainer</h2>
+      </div>
+
+      <div>
+        <h1>{counter}</h1>
+        <button className="btn" onClick={handleClick}>
+          Click me
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
