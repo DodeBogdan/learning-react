2daaa0d27284258f8fd1b477b406eafbf4886eb9
Bogdan Gheorghe <dodebogdan@yahoo.com>
2023-02-21 21:18:46 +0200

    Created birthday component

--- src/learning-components/birthday_project/Birthday.jsx
+++ src/learning-components/birthday_project/Birthday.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Birthday.css";
+import birthdayList from "./birthdaysData";
 
 const Birthday = () => {
-  return <div>Hapipi birthday</div>;
+  const [birthdays, setBirthdays] = useState(birthdayList);
+  const [currentPage, setCurrentPage] = useState(0);
+  const maxPerPage = 3;
+  const numberOfPages = Math.ceil(birthdays.length / maxPerPage);
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="birthday">
+      <section className="panel">
+        <h2>Happy birthdays</h2>
+        {birthdays
+          .slice(
+            currentPage * maxPerPage,
+            currentPage * maxPerPage + maxPerPage
+          )
+          .map((birthday) => {
+            const { id, date, name } = birthday;
+            return (
+              <div key={id}>
+                <h3>Name: {name}</h3>
+                <h4>Date: {date}</h4>
+              </div>
+            );
+          })}
+        <div>
+          Page:
+          {[...Array(numberOfPages)].map((_, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => {
+                  setCurrentPage(index);
+                }}
+                className="btn"
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+        </div>
+        <button type="button" className="btn mb mt">
+          Clear all
+        </button>
+      </section>
+    </div>
+  );
 };
 
 export default Birthday;
---
+++ src/learning-components/birthday_project/birthdaysData.js
@@ -0,0 +1,12 @@
+export const birthdays = [
+  { id: 1, date: "19 ianuarie", name: "Sebi" },
+  { id: 2, date: "12 februarie", name: "Simo" },
+  { id: 3, date: "19 martie", name: "Bianca" },
+  { id: 4, date: "2 iunie", name: "Dode" },
+  { id: 5, date: "14 iunie", name: "Cata" },
+  { id: 6, date: "8 august", name: "Silvia" },
+  { id: 7, date: "1 octombrie", name: "Andrada" },
+  { id: 8, date: "23 noiembrie", name: "Nicusor" },
+];
+
+export default birthdays;
